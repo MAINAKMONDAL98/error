1274ddb39c326800474de4a693854f2486a5a688
MAINAK MONDAL <32256636+MAINAKMONDAL98@users.noreply.github.com>
2019-07-18 00:32:47 +0530

    Update error .cpp

--- error .cpp
+++ error .cpp
@@ -1,37 +1,44 @@
-#include<iostream>
-#include<string>
-#include<sstream>
+//n,r,c,sr,sc,new
+#include <iostream> 
+#include <sstream>
+#include <string>
 using namespace std;
-int main()
-{
-int cs;
-cin>>cs;
-for(int R=0;R<cs;R++){
-
+int main() {
 int po[5];
 float valu;
-string str1;
-string temp1;
-getline(cin,str1);
+std::string str1;
+std::string temp1;
+int cs;
+cin>>cs;
+for(int R=0;R<=cs;R++){
+	
+cout<<"I am R"<<R<<endl;
+
+std::getline(std::cin,str1);
 int x2=-1;
-stringstream ss(str1);
+std::stringstream ss(str1);
+//cout<<"hello";
 while(getline(ss,temp1, ' '))
-{	  x2++;
-      stringstream stream(temp1);
+{x2++;
+      std::stringstream stream(temp1);
       if(stream >> valu)
 {
+
+        //std::cout<<val<<std::endl;
+        //std::cout<<val+1<<std::endl;
 		po[x2]=valu;
-        //cout<<po[x2]<<endl;;
+        cout<<valu<<endl;
 		}
       else 
-        cout<<temp1<<std::endl;
+        std::cout<<temp1<<std::endl;
 }
-//cout<<p[4]+1<<endl;
-//cin >> n>>r>>c>>sr>>sc;
 int n=po[0];
 int r=po[1];
+int c=po[2];
 int sr=po[3];
 int sc=po[4];
 cout<<n<<sr<<endl;
-return 0;}
+
+}
+//cout<<"hello";
 }
